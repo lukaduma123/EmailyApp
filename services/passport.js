cb530b643131e0e8e45f46d11e8d1129ced029dc
Luka Dumancic <luka.dumancic49@gmail.com>
2018-12-18 17:29:55 +0100

    Heroku react deployment

--- services/passport.js
+++ services/passport.js
@@ -23,15 +23,16 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({googleID: profile.id}).then((existingUser) => {
-        if (existingUser) {
-          //User exists
-          done(null, existingUser);
-        } else {
-          new User({googleID: profile.id}).save().then((user) => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      let existingUser = await User.findOne({googleID: profile.id});
+      if (existingUser) {
+        //User exists
+        done(null, existingUser);
+      } else {
+        //New user
+        let user = await new User({googleID: profile.id}).save();
+        done(null, user);
+      }
     }
   )
 );
