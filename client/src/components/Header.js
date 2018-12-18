c1922eadd554eb32151cac46888643244db313bc
Luka Dumancic <luka.dumancic49@gmail.com>
2018-12-18 17:19:02 +0100

    Payment added

---
+++ client/src/components/Header.js
@@ -0,0 +1,46 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {Link} from "react-router-dom";
+import Payments from "./Payments";
+
+class Header extends Component {
+  render() {
+    console.log(this.props.auth);
+    return (
+      <nav>
+        <div className='nav-wrapper'>
+          <Link to={this.props.auth ? "/surveys" : "/"} className='brand-logo'>
+            Emaily
+          </Link>
+          <ul id='nav-mobile' className='right'>
+            {this.props.auth === null ? (
+              <p>Loading</p>
+            ) : this.props.auth === false ? (
+              <li>
+                <a href='/auth/google'>Google login</a>
+              </li>
+            ) : (
+              [
+                <li key='1'>
+                  <Payments />
+                </li>,
+                <li key='2' style={{margin: "0 10px"}}>
+                  Credits : {this.props.auth.credits}
+                </li>,
+                <li key='3'>
+                  <a href='/buy'>Buy</a>
+                </li>
+              ]
+            )}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps({auth}) {
+  return {auth};
+}
+
+export default connect(mapStateToProps)(Header);
